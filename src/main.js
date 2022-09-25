54ea6f48a8cce7406f5c67cb8f44cbf84fa05549
Quintin Henn <quintin.henn@gmail.com>
2022-09-25 08:11:30 +0200

    Start app only when cordova is done loading

--- src/main.js
+++ src/main.js
@@ -8,13 +8,6 @@ import "normalize.css";
 import "bulma/css/bulma.css";
 import "./assets/css/main.css";
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
-
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -23,4 +16,11 @@ function onDeviceReady() {
   // Cordova is now initialized. Have fun!
 
   console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+
+  const app = createApp(App);
+
+  app.use(createPinia());
+  app.use(router);
+
+  app.mount("#app");
 }
