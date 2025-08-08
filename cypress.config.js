d8c3656829096637a9eeb5ef9849aeae86e9e5b0
Quintin Henn <quintin.henn@gmail.com>
2025-08-08 08:48:53 +0200

    feat: upgraded cypress to latest version

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,11 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
     setupNodeEvents(on, config) {
       // e2e testing node events setup code
-    },
-  },
-  component: {
-    setupNodeEvents(on, config) {
-      // component testing node events setup code
     },
   },
 })
